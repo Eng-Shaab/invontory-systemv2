a6c68c74960411929cf76a9cfdf10052578e86a0
eng-shaab <shaab1012@gmail.com>
2025-10-30 13:43:12 +0300

    update

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,26 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { clerkMiddleware } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/products(.*)",
-  "/purchases(.*)",
-  "/customers(.*)",
-  "/sales(.*)",
-  "/inventory(.*)",
-  "/settings(.*)",
-  "/test(.*)",
-])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect()
-  }
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Match all routes except Next.js internals and static assets
+    "/((?!_next|static|favicon.ico).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
