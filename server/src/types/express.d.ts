6bf2b6fbd949f100034a9c58c7174754c642dc5a
eng-shaab <shaab1012@gmail.com>
2025-11-08 16:06:55 +0300

    fix colors

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,12 +1,2 @@
-import type { AuthenticatedUser } from "./auth";
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: AuthenticatedUser;
-      sessionId?: string;
-    }
-  }
-}
-
-export {};
+// Moved to ./express/index.d.ts to align with TypeScript typeRoots expectations.
+export {}
